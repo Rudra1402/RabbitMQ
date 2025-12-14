fc35b64f24655e06545e4bfcb68d13f40d1651e5
Rudra Patel <78080396+Rudra1402@users.noreply.github.com>
2025-12-14 00:54:26 -0500

    feat(RabbitMQ): Implement Multi-Queue Consumers Code | Folder Structure (#2)

---
+++ multiQueue/consumerUnsubscribed.js
@@ -0,0 +1,46 @@
+// Modules ------------------------------------------->
+
+const amqp = require("amqplib");
+
+// Globals ------------------------------------------->
+
+const RABBITMQ_URL = "amqp://localhost";
+const QUEUE_NAME_UNSUBSCRIBED = "email_queue_unsubscribed";
+
+// Methods ------------------------------------------->
+
+async function receiveEmail() {
+    try {
+        // Connect to amqp server
+        const connection = await amqp.connect(RABBITMQ_URL);
+        if (!connection) {
+            console.log("[ ERROR ] Failed to connect to " + RABBITMQ_URL);
+            return;
+        }
+
+        // Create a channel
+        const channel = await connection.createChannel();
+        if (!channel) {
+            console.log("[ ERROR ] Failed to create a channel");
+            return;
+        }
+
+        // Assert queue
+        await channel.assertQueue(QUEUE_NAME_UNSUBSCRIBED, {
+            durable: false
+        });
+
+        channel.consume(QUEUE_NAME_UNSUBSCRIBED, (message) => {
+            if(!message || !message.content) {
+                console.log("[ ERROR ] Failed to consume messages from queue: " + QUEUE_NAME_UNSUBSCRIBED);
+                return;
+            }
+            console.log("[ INFO ] Successfully received data for unsubscribed users: " + message.content);
+            channel.ack(message);
+        });
+    } catch (error) {
+        
+    }
+}
+
+receiveEmail();
